2577d1eb44a347374319edf75a0e6c07c3bdfcd0
Maksim Kolyadin <makcyxa-k@yandex.ru>
2024-02-22 19:05:23 +0300

    Remove PaymentType

--- back/app/Models/ClientPayment.php
+++ back/app/Models/ClientPayment.php
@@ -4,14 +4,12 @@ namespace App\Models;
 
 use App\Enums\ClientPaymentMethod;
 use App\Enums\CompanyType;
-use App\Enums\PaymentType;
 use Illuminate\Database\Eloquent\Model;
 
 class ClientPayment extends Model
 {
     protected $casts = [
         'company' => CompanyType::class,
-        'type' => PaymentType::class,
         'method' => ClientPaymentMethod::class,
         'year' => 'integer',
     ];
