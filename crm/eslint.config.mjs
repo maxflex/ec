126ab3d56f25271568310bfd705084c64c076f9c
maxflex <makcyxa-k@yandex.ru>
2024-05-20 16:54:58 +0300

    Prettier-ignore

--- crm/eslint.config.mjs
+++ crm/eslint.config.mjs
@@ -1,6 +1,10 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import stylistic from '@stylistic/eslint-plugin'
+
 
 export default withNuxt(
   // Your custom configs here
+).prepend(
+  stylistic.configs['recommended-flat']
 )
