2618e2af2a6f135ff4733bc1a19d993ef775d422
maxflex <makcyxa-k@yandex.ru>
2024-06-26 12:09:55 +0300

    DateInput anim

--- crm/components/Ui/DateInput.vue
+++ crm/components/Ui/DateInput.vue
@@ -21,6 +21,9 @@ const calendarDialog = ref()
       :model-value="model ? formatDate(model) : null"
       hide-details
     />
+    <v-icon
+      icon="$next"
+    />
   </div>
   <CalendarDialog
     ref="calendarDialog"
@@ -31,8 +34,26 @@ const calendarDialog = ref()
 <style lang="scss">
 .date-input {
   cursor: pointer;
+  position: relative;
   .v-input {
     pointer-events: none;
+  }
+  & > .v-icon {
+    opacity: 0;
+    // opacity: 0.6;
+    position: absolute;
+    top: 16px;
+    right: 12px;
+    transition: all 0.2s cubic-bezier(0.2, 0, 0.4, 0.9);
+  }
+  &:hover {
+    .v-field__outline {
+      --v-field-border-opacity: var(--v-high-emphasis-opacity);
+    }
+    & > .v-icon {
+      opacity: 0.6;
+      right: 6px;
+    }
   }
 }
 </style>
