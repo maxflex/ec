e74a2e989d0686ce33383f469e9d0b7be9ce201f
maxflex <makcyxa-k@yandex.ru>
2024-10-31 12:49:55 +0300

    Transfer quarters

--- back/app/Console/Commands/Transfer/TransferLessons.php
+++ back/app/Console/Commands/Transfer/TransferLessons.php
@@ -3,8 +3,11 @@
 namespace App\Console\Commands\Transfer;
 
 use App\Enums\Cabinet;
+use App\Enums\Quarter;
 use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 class TransferLessons extends Command
 {
@@ -15,11 +18,16 @@ class TransferLessons extends Command
 
     public function handle()
     {
+        Schema::disableForeignKeyConstraints();
         DB::table('lessons')->delete();
         $lessons = DB::connection('egecrm')
             ->table('lessons', 'l')
             ->join('groups as g', 'g.id', '=', 'l.group_id')
-            ->selectRaw('l.*, if(g.is_free, 1, l.is_free) as is_free')
+            ->selectRaw("
+                l.*, 
+                if(g.is_free, 1, l.is_free) as is_free,
+                g.year
+            ")
             ->get();
         $bar = $this->output->createProgressBar($lessons->count());
         foreach ($lessons as $l) {
@@ -32,6 +40,7 @@ class TransferLessons extends Command
                 'status' => $l->status,
                 'date' => $l->date,
                 'time' => $l->time,
+                'quarter' => $this->getQuarter($l),
                 'conducted_at' => $l->conducted_at,
                 'is_free' => $l->is_free,
                 'is_unplanned' => $l->is_unplanned,
@@ -45,5 +54,32 @@ class TransferLessons extends Command
             $bar->advance();
         }
         $bar->finish();
+        Schema::enableForeignKeyConstraints();
+    }
+
+    private function getQuarter($l): ?Quarter
+    {
+        $year = intval($l->year);
+        if ($year !== 2023) {
+            return null;
+        }
+        $lessonDate = $l->date;
+        $nextYear = $year + 1;
+        $quarters = [
+            "$year-11-15",
+            "$year-12-30",
+            "$nextYear-03-15",
+//            "$nextYear-05-30",
+        ];
+
+        // Определяем четверть, в которую попадает дата занятия
+        foreach ($quarters as $index => $quarterEndDate) {
+            if ($lessonDate <= $quarterEndDate) {
+                $q = $index + 1;
+                return Quarter::from("q$q");
+            }
+        }
+
+        return Quarter::q4;
     }
 }
