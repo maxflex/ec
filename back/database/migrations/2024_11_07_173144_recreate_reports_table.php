03492f8ff4042bb91f771ddf614dde20b3a33afe
maxflex <makcyxa-k@yandex.ru>
2024-11-07 19:15:26 +0300

    Recreate reports

--- back/database/migrations/2024_11_07_173144_recreate_reports_table.php
+++ back/database/migrations/2024_11_07_173144_recreate_reports_table.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Enums\Program;
+use App\Enums\ReportStatus;
+use App\Models\Client;
+use App\Models\Teacher;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,7 +15,31 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::dropIfExists('reports');
+        Schema::create('reports', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Teacher::class)->constrained();
+            $table->foreignIdFor(Client::class)->constrained();
+            $table->unsignedSmallInteger('year')->index();
+            $table->enum(
+                'program',
+                collect(Program::cases())->map(fn($e) => $e->name)->all()
+            )->index();
+            $table->mediumText('homework_comment')->nullable();
+            $table->mediumText('cognitive_ability_comment')->nullable();
+            $table->mediumText('knowledge_level_comment')->nullable();
+            $table->mediumText('recommendation_comment')->nullable();
+            $table->unsignedSmallInteger('price')->nullable();
+            $table->unsignedSmallInteger('grade')->nullable();
+            $table->enum(
+                'status',
+                array_column(ReportStatus::cases(), 'value')
+            )->default(ReportStatus::new)->index();
+            $table->timestamps();
+        });
+        Schema::table('reports', function (Blueprint $table) {
+            $table->index('created_at'); // Add index to the created_at column
+        });
     }
 
     /**
