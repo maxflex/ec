a465f8d538044fc80fef6e8f42e4a8319e2836fa
maxflex <makcyxa-k@yandex.ru>
2024-12-16 10:54:32 +0300

    Transfer zoom

---
+++ back/app/Console/Commands/Transfer/TransferZoom.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands\Transfer;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * Добавление после основного переноса
+ */
+class TransferZoom extends Command
+{
+    use TransferTrait;
+
+    protected $signature = 'app:transfer:zoom';
+    protected $description = 'Transfer zoom';
+
+    public function handle()
+    {
+        $groups = DB::connection('egecrm')
+            ->table('groups')
+            ->whereNotNull('zoom_id')
+            ->get();
+        $bar = $this->output->createProgressBar($groups->count());
+        foreach ($groups as $g) {
+            DB::table('groups')->whereId($g->id)->update([
+                'zoom' => $g->zoom_id ? json_encode([
+                    'id' => $g->zoom_id,
+                    'password' => $g->zoom_password
+                ]) : null
+            ]);
+            $bar->advance();
+        }
+        $bar->finish();
+        Schema::enableForeignKeyConstraints();
+    }
+}
