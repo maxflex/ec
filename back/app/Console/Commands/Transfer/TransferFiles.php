f1a19fec57d9d4f9417f008c9b33a85e4d2fb88d
maxflex <makcyxa-k@yandex.ru>
2025-01-15 17:25:55 +0300

    Transfer files fix

--- back/app/Console/Commands/Transfer/TransferFiles.php
+++ back/app/Console/Commands/Transfer/TransferFiles.php
@@ -2,9 +2,9 @@
 
 namespace App\Console\Commands\Transfer;
 
+use App\Models\Lesson;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Storage;
 
 class TransferFiles extends Command
@@ -16,12 +16,47 @@ class TransferFiles extends Command
 
     public function handle()
     {
-        DB::table('lessons')->update([
-            'files' => null
-        ]);
+        $this->deleteExistingFiles();
+        $this->uploadFilesFromV2();
+    }
 
-        // delete all files in the dir
-        Storage::delete(Storage::files('crm/lessons'));
+    /**
+     * удалить ранее загруженные фалы (нужно для перезапуска команды)
+     */
+    private function deleteExistingFiles()
+    {
+        $this->info("Removing existing files...");
+        $this->line(PHP_EOL);
+
+        $query = Lesson::query()
+            ->where('date', '<=', '2024-12-15')
+            ->whereNotNull('files');
+
+        $lessonsWithFiles = $query->get();
+
+        $bar = $this->output->createProgressBar($lessonsWithFiles->count());
+
+        foreach ($lessonsWithFiles as $lesson) {
+            foreach ($lesson->files as $file) {
+                if (isset($file->url)) {
+                    $file = str($file->url)->after('https://cdn.ege-centr.ru/')->value();
+                    Storage::delete($file);
+                }
+            }
+            $bar->advance();
+        }
+
+        $bar->finish();
+
+        $query->update(['files' => null]);
+    }
+
+
+    private function uploadFilesFromV2()
+    {
+        $this->info("Uploading files...");
+        $this->line(PHP_EOL);
+
 
         $files = DB::connection('egecrm')
             ->table('files', 'f')
@@ -29,6 +64,7 @@ class TransferFiles extends Command
             ->join('groups as g', 'g.id', '=', 'l.group_id')
             ->where('f.entity_type', self::ET_LESSON)
             ->whereIn('g.year', [2023, 2024])
+            ->where('l.date', '<=', '2024-12-15')
             ->selectRaw('f.*')
             ->get()
             ->groupBy('entity_id');
@@ -37,7 +73,7 @@ class TransferFiles extends Command
         foreach ($files as $lessonId => $lessonFiles) {
             $json = [];
             foreach ($lessonFiles as $lessonFile) {
-                $file = file_get_contents("https://lk.ege-centr.ru/storage/img/upload/{$lessonFile->name}");
+                $file = file_get_contents("https://v2.ege-centr.ru/storage/img/upload/{$lessonFile->name}");
                 Storage::put("crm/lessons/{$lessonFile->name}", $file);
                 $json[] = [
                     'url' => cdn('lessons', $lessonFile->name),
@@ -51,7 +87,6 @@ class TransferFiles extends Command
             $bar->advance();
         }
         $bar->finish();
-        Schema::enableForeignKeyConstraints();
     }
 }
 /**
