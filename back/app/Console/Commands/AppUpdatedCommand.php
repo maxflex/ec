9d08752ca6b0c540825e279190f62b6f3d290b85
maxflex <makcyxa-k@yandex.ru>
2025-02-21 16:32:01 +0300

    CI/CD test

--- back/app/Console/Commands/AppUpdatedCommand.php
+++ back/app/Console/Commands/AppUpdatedCommand.php
@@ -14,7 +14,7 @@ class AppUpdatedCommand extends Command
     public function handle(): void
     {
         AppUpdatedEvent::dispatch([
-            // TODO: clear filters
+            // TODO: clear filters1
             'filters-strings',
         ]);
     }
