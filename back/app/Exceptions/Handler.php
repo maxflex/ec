d4ee34acf470540ed5dfe4d20a3fd8e3ff3c794c
maxflex <makcyxa-k@yandex.ru>
2025-04-14 17:00:04 +0300

    Show 429 Errors in BugSnag

--- back/app/Exceptions/Handler.php
+++ back/app/Exceptions/Handler.php
@@ -2,7 +2,9 @@
 
 namespace App\Exceptions;
 
+use Bugsnag\BugsnagLaravel\Facades\Bugsnag;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Exceptions\ThrottleRequestsException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -26,5 +28,23 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $e)
+    {
+        if ($e instanceof ThrottleRequestsException) {
+            Bugsnag::notifyException($e, function ($report) use ($request) {
+                $report->setMetaData([
+                    'request' => [
+                        'ip' => $request->ip(),
+                        'url' => $request->fullUrl(),
+                        'method' => $request->method(),
+                        'body' => $request->all(),
+                    ],
+                ]);
+            });
+        }
+
+        return parent::render($request, $e);
     }
 }
