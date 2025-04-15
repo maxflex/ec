89c5e1324d345b602d6485720f81f7db5eb29207
maxflex <makcyxa-k@yandex.ru>
2025-04-15 16:37:53 +0300

    Tests page is back

---
+++ crm/pages/admin/tests/index.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import type { TestDialog } from '#build/components'
+import type { TestResource } from '~/components/Test'
+
+const testDialog = ref<InstanceType<typeof TestDialog>>()
+const tests = ref<TestResource[]>()
+
+async function loadData() {
+  const { data } = await useHttp<ApiResponse<TestResource>>(`tests`)
+  if (data.value) {
+    const { data: newItems } = data.value
+    tests.value = newItems
+  }
+}
+
+nextTick(loadData)
+</script>
+
+<template>
+  <div class="tests">
+    <UiTopPanel>
+      <v-spacer />
+      <v-btn
+        append-icon="$next"
+        color="primary"
+        @click="testDialog?.create()"
+      >
+        добавить тест
+      </v-btn>
+    </UiTopPanel>
+    <TestList
+      v-if="tests"
+      :tests="tests"
+      @open="testDialog?.open"
+    />
+  </div>
+  <TestDialog
+    ref="testDialog"
+    @updated="loadData()"
+  />
+</template>
