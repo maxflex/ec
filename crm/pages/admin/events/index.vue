c6201ea1e7550fb4a5c9369aff804d8f4d4d8d78
maxflex <makcyxa-k@yandex.ru>
2025-05-21 17:32:05 +0300

    Event index clean-up

--- crm/pages/admin/events/index.vue
+++ crm/pages/admin/events/index.vue
@@ -1,84 +1,23 @@
 <script setup lang="ts">
 import type { EventDialog } from '#components'
 
-interface Filters {
-  year: Year
-}
-
-const loading = ref(false)
-const items = ref<EventListResource[]>([])
-const filters = ref<Filters>(loadFilters({
+const filters = ref<YearFilters>(loadFilters({
   year: currentAcademicYear(),
 }))
 const eventDialog = ref<InstanceType<typeof EventDialog>>()
-let scrollContainer: HTMLElement | null = null
-
-async function loadData() {
-  if (loading.value) {
-    return
-  }
-  loading.value = true
-  const { data } = await useHttp<ApiResponse<EventListResource>>(
-    `events`,
-    {
-      params: {
-        ...filters.value,
-      },
-    },
-  )
-  if (data.value) {
-    const { data: newItems } = data.value
-    items.value = newItems
-  }
-  loading.value = false
-}
-
-function onFiltersApply(f: Filters) {
-  filters.value = f
-  if (scrollContainer) {
-    scrollContainer.scrollTop = 0
-  }
-  loadData()
-}
-
-watch(filters, (newVal) => {
-  saveFilters(newVal)
-  onFiltersApply(newVal)
-}, { deep: true })
-
-function onUpdated(e: EventListResource) {
-  const index = items.value.findIndex(x => x.id === e.id)
-  if (index !== -1) {
-    items.value[index] = e
-  }
-  else {
-    items.value.unshift(e)
-  }
-  itemUpdated('event', e.id)
-}
-
-function onDeleted(e: EventResource) {
-  const index = items.value.findIndex(x => x.id === e.id)
-  if (index !== -1) {
-    items.value.splice(index, 1)
-  }
-}
-
-onMounted(() => {
-  scrollContainer = document.documentElement.querySelector('main')
-})
-
-nextTick(loadData)
+const { items, indexPageData } = useIndex<EventListResource>(`events`, filters)
 </script>
 
 <template>
-  <UiFilters>
-    <v-select
-      v-model="filters.year"
-      label="Учебный год"
-      :items="selectItems(YearLabel)"
-      density="comfortable"
-    />
+  <UiIndexPage :data="indexPageData">
+    <template #filters>
+      <v-select
+        v-model="filters.year"
+        label="Учебный год"
+        :items="selectItems(YearLabel)"
+        density="comfortable"
+      />
+    </template>
     <template #buttons>
       <v-btn
         append-icon="$next"
@@ -88,13 +27,11 @@ nextTick(loadData)
         добавить событие
       </v-btn>
     </template>
-  </UiFilters>
-  <div>
     <EventList
       :items="items"
       :year="filters.year"
       @edit="eventDialog?.edit"
     />
-  </div>
-  <EventDialog ref="eventDialog" @updated="onUpdated" @deleted="onDeleted" />
+  </UiIndexPage>
+  <LazyEventDialog ref="eventDialog" />
 </template>
