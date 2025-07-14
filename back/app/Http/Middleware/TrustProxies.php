78203147eaed7fa7159d217fd6283ebd86824012
maxflex <makcyxa-k@yandex.ru>
2025-07-14 10:56:07 +0300

    Update TrustProxies.php

--- back/app/Http/Middleware/TrustProxies.php
+++ back/app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,9 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    protected $proxies = [
+        '37.140.195.195',
+    ];
 
     /**
      * The headers that should be used to detect proxies.
