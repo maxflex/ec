f8d6ae6387d249a2593b124d23ec123a38abb6bc
maxflex <makcyxa-k@yandex.ru>
2026-02-13 15:19:36 +0300

    Restrict macro list response fields

--- back/app/Http/Controllers/Admin/MacroController.php
+++ back/app/Http/Controllers/Admin/MacroController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\MacroListResource;
 use App\Http\Resources\MacroResource;
 use App\Models\Macro;
 use Illuminate\Http\Request;
@@ -11,13 +12,17 @@ class MacroController extends Controller
 {
     public function index(Request $request)
     {
-        $query = Macro::orderBy('title');
-        return $this->handleIndexRequest($request, $query, MacroResource::class);
+        // Для списка используем отдельный ListResource и лёгкую выборку.
+        $query = Macro::query()
+            ->select(['id', 'title'])
+            ->orderBy('title');
+
+        return $this->handleIndexRequest($request, $query, MacroListResource::class);
     }
 
     public function show(Macro $macro)
     {
-        return $macro;
+        return new MacroResource($macro);
     }
 
     public function update(Macro $macro, Request $request)
---
+++ back/app/Http/Resources/MacroListResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\Macro;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @mixin Macro
+ */
+class MacroListResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return extract_fields($this, ['id', 'title']);
+    }
+}
