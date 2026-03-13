ac1b0084d1e3df19bf80aa669462c1678445c10c
maxflex <makcyxa-k@yandex.ru>
2026-03-13 16:07:31 +0300

    [wip] New phone menu

--- crm/components/Phone/List.vue
+++ crm/components/Phone/List.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import type { PhoneDialog } from '#build/components'
 import type { RequestListResource } from '../Request'
+import { mdiChevronRight, mdiClipboardOutline, mdiHistory, mdiPhoneOutgoing } from '@mdi/js'
 
 const { items, request, showComment, person } = defineProps<{
   items: PhoneResource[]
@@ -8,53 +8,61 @@ const { items, request, showComment, person } = defineProps<{
   person?: PersonResource
   showComment?: boolean
 }>()
-
-const phoneDialog = ref<InstanceType<typeof PhoneDialog>>()
-
-// подсвечиваем номер телефона
-// function highlightPhone(number: string) {
-//   const text = formatPhone(number)
-//   const index = number.indexOf(q!)
-//   if (index === -1) {
-//     return text
-//   }
-//   // 79252727210 => +7 (925) 272-11-22
-//   const indexMap = [1, 4, 5, 6, 9, 10, 11, 13, 14, 16, 17]
-//   const start = indexMap[index]
-//   const end = indexMap[index + q!.length - 1]
-//   const newQ = text.substring(start, end - start + 1)
-//   return highlight(text, newQ)
-// }
-
-// Подсветить результаты поиска. Временно
-// function highlight(text: string, _q: string) {
-//   let result = text.trim()
-//   const qWords = _q.split(' ').filter(Boolean)
-//
-//   for (const word of qWords) {
-//     if (!word)
-//       continue
-//
-//     const newQ = word.replace(')', '\\)').replace('(', '\\(')
-//     result = result.replace(
-//       new RegExp(`${newQ}`, 'gi'),
-//       `<span class="highlight">${word}</span>`,
-//     )
-//   }
-//
-//   return result
-// }
 </script>
 
 <template>
   <div class="phone-list">
     <div v-for="item in items" :key="item.id">
-      <PhoneNumber
-        :item="item"
-        class="phone-list__number"
-        :request="request"
-        @click.stop="phoneDialog?.open(item, person)"
-      />
+      <!-- transition="slide-y-transition" -->
+      <v-menu
+        location="bottom center"
+        offset="10"
+      >
+        <template #activator="{ props }">
+          <PhoneNumber
+            :item="item"
+            class="phone-list__number"
+            :request="request"
+            v-bind="props"
+          />
+        </template>
+        <v-list class="phone-list__list">
+          <v-list-item>
+            <v-icon :icon="mdiPhoneOutgoing" />
+            позвонить
+          </v-list-item>
+          <v-list-item>
+            <v-icon :icon="mdiClipboardOutline" />
+            скопировать номер
+          </v-list-item>
+          <v-list-item link>
+            <v-icon :icon="mdiHistory" />
+            история
+            <v-icon :icon="mdiChevronRight" class="phone-list__expand" />
+            <v-menu
+              :open-on-focus="false"
+              activator="parent" submenu location="right center" transition="slide-x-transition"
+            >
+              <v-list>
+                <v-list-item link>
+                  история звонков
+                </v-list-item>
+                <v-list-item link>
+                  история telegram
+                </v-list-item>
+                <v-list-item link>
+                  история SMS
+                </v-list-item>
+              </v-list>
+            </v-menu>
+          </v-list-item>
+          <v-divider />
+          <v-list-item>
+            <v-icon icon="$preview" />
+            войти в лк
+          </v-list-item>
+        </v-list>
+      </v-menu>
       <div
         v-if="showComment"
         class="phone-list__comment"
@@ -63,20 +71,44 @@ const phoneDialog = ref<InstanceType<typeof PhoneDialog>>()
       </div>
     </div>
   </div>
-  <PhoneDialog ref="phoneDialog" />
 </template>
 
 <style lang="scss">
 .phone-list {
+  overflow: visible;
   & > div {
     display: flex;
+    overflow: visible;
   }
   &__number {
     cursor: pointer;
     width: 160px;
+    position: relative;
+    overflow: visible;
+    z-index: 1;
+
     //&:hover {
     //  color: rgb(var(--v-theme-secondary)) !important;
     //}
+
+    &:after {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      height: 100%;
+      width: 140px;
+      background-color: transparent;
+      z-index: -1;
+      border-radius: 4px;
+      transition: all ease-in-out 0.3s;
+    }
+    &[aria-expanded='true'] {
+      &:after {
+        background-color: rgba(var(--v-theme-primary), 0.2);
+        box-shadow: 0 0 3px 3px rgba(var(--v-theme-primary), 0.2);
+      }
+    }
   }
   &__comment {
     color: rgb(var(--v-theme-gray));
@@ -85,6 +117,24 @@ const phoneDialog = ref<InstanceType<typeof PhoneDialog>>()
     display: inline-block;
     white-space: nowrap;
     width: 120px;
+  }
+
+  &__list {
+    .v-list-item {
+      .v-icon {
+        margin-right: 6px;
+      }
+    }
+  }
+
+  &__expand {
+    $size: 20px;
+    margin: 0 !important;
+    position: absolute;
+    right: 6px;
+    top: calc(50% - #{$size / 2});
+    color: rgb(var(--v-theme-gray));
+    font-size: $size;
   }
 }
 </style>
